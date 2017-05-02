4ca02fd1cc20f754dc884e3650e493267e5ab567
Narek <narekelbakyan@gmail.com>
2017-05-02 22:46:54 +0400

    asdasdas

--- wp-content/themes/life-style/page-blog.php
+++ wp-content/themes/life-style/page-blog.php
@@ -7,49 +7,12 @@
  */
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+    <?php
 
-            <img src="<?= get_template_directory_uri()?>/images/UnderConstruction.png" alt="">
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+wp_nav_menu( array(
+    'menu' => 'Blog Category Menu'
+) );
+?>
 
-    <section class="idea-earn">
-        <div class="item">
-            <img src="<?= get_template_directory_uri()?>/images/icon-flower.png" alt="">
-            <div class="title">SUBMIT YOUR IDEA</div>
-            <div class="text">Got a brilliant idea of a quote to motivate and inspire?
-                If you believe in your idea to be the great one,
-                share it with us and earn royalties from the sales.</div>
-            <a href="">Submit Now</a>
-        </div>
-        <div class="item">
-            <img src="<?= get_template_directory_uri()?>/images/icon-wallet.png" alt="">
-            <div class="title">EMAIL US YOUR GREAT IDEAS  & EARN</div>
-            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-            <a href="">Click Here</a>
-        </div>
-        <div class="clear"></div>
-    </section>
-    <section class="gift">
-        <div class="wrapper">
-            <div class="box">
-                <div class="line">
-                    <div class="dash"></div>
-                </div>
-                <div class="content">
-                    <div class="box-wrap">
-                        <img src="<?= get_template_directory_uri()?>/images/icon_present.png">
-                        <div class="title">BUY IT AS A GIFT</div>
-                        <div class="text">When you choose <span>HUSTLE LIFESTYLE</span> products as a gift
-                            you present not just a t-shirt, along you give <span>an Inspiration</span>.
-                            Just choose it and press GIFT button. We will deliver your gift directly to the address of your friend and along with the personalized message.
-                        </div>
-                        <a href="">Free Gift Packaging</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 <?php get_footer();
---
+++ wp-content/themes/life-style/taxonomy-blog_category.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: narek
+ * Date: 02.05.2017
+ * Time: 20:43
+ */
+get_header(); ?>
+
+<div class="blog-category-menu">
+   <div class="wrapper">
+       <?php
+       wp_nav_menu( array(
+           'menu' => 'Blog Category Menu'
+       ) );
+       ?>
+   </div>
+</div>
+    <section class="blog-posts">
+        <div class="wrapper">
+            <?php
+            $Info = get_the_terms($post->ID, 'blog_category');
+
+
+            $type = 'blog';
+            $args=array(
+                'post_type' => $type,
+                'post_status' => 'publish',
+                'posts_per_page' => 6,
+                'orderby' => 'rand',
+                'tax_query' 			=> array(
+                    array(
+                        'taxonomy' 		=> 'blog_category',
+                        'terms' 		=> $Info[0]->slug,
+                        'field' 		=> 'slug',
+                        'operator' 		=> 'IN'
+                    )
+                )
+            );
+            $my_query = null;
+            $my_query = new WP_Query($args);
+            if( $my_query->have_posts() ) {
+                while ($my_query->have_posts()) : $my_query->the_post();
+                    $Info = get_the_terms($post->ID, 'blog_category'); ?>
+                    <div class="blog-item <?php echo $Info[0]->slug?>">
+                        <a href="<?= esc_url(get_permalink()) ?>" class="permalink">
+                            <?php the_post_thumbnail()?>
+                            <div class="box">
+                                <div class="title"><?php the_title()?></div>
+                                <div class="date"><?php the_time('d.m.y G:i:s'); ?></div>
+                                <?php $summary = get_field('post_short_description');?>
+                                <div class="short-description"><?= substr($summary,0 , 120) . '...' ;?></div>
+                                <div class="read-more">read more</div>
+                            </div>
+                        </a>
+                    </div>
+                    <?php
+
+                endwhile;
+            }
+            wp_reset_query();  // Restore global post data stomped by the_post().
+
+            ?>
+        </div>
+    </section>
+    <section class="idea-earn">
+        <div class="item">
+            <img src="<?= get_template_directory_uri()?>/images/icon-flower.png" alt="">
+            <div class="title">SUBMIT YOUR IDEA</div>
+            <div class="text">Got a brilliant idea of a quote to motivate and inspire?
+                If you believe in your idea to be the great one,
+                share it with us and earn royalties from the sales.</div>
+            <a href="">Submit Now</a>
+        </div>
+        <div class="item">
+            <img src="<?= get_template_directory_uri()?>/images/icon-wallet.png" alt="">
+            <div class="title">EMAIL US YOUR GREAT IDEAS  & EARN</div>
+            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
+            <a href="">Click Here</a>
+        </div>
+        <div class="clear"></div>
+    </section>
+    <section class="gift">
+        <div class="wrapper">
+            <div class="box">
+                <div class="line">
+                    <div class="dash"></div>
+                </div>
+                <div class="content">
+                    <div class="box-wrap">
+                        <img src="<?= get_template_directory_uri()?>/images/icon_present.png">
+                        <div class="title">BUY IT AS A GIFT</div>
+                        <div class="text">When you choose <span>HUSTLE LIFESTYLE</span> products as a gift
+                            you present not just a t-shirt, along you give <span>an Inspiration</span>.
+                            Just choose it and press GIFT button. We will deliver your gift directly to the address of your friend and along with the personalized message.
+                        </div>
+                        <a href="">Free Gift Packaging</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php get_footer();
